e74bce1ae47eb1f41bc7deaac312f5d1ec297104
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-12-06 18:23:00 -0300

    Update to new version

---
+++ API_KEY.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5tmpVHFUIDW7nq87eJv57BgKW54G5iwI",
+    authDomain: "ipro-database.firebaseapp.com",
+    projectId: "ipro-database",
+    storageBucket: "ipro-database.appspot.com",
+    messagingSenderId: "649052552256",
+    appId: "1:649052552256:web:30aae1b3568760fd74b3a5",
+    measurementId: "G-VLVDDEQTWL"
+  };
+
+export default firebaseConfig;
